4e47485942fdec02c891fdd5abd91062b397994f
Josefina Arreola <maya_aiji.11@hotmail.com>
2021-04-27 14:50:56 -0700

    Agregar archivos de proyecto.

---
+++ Arreglo3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglo3
+{
+    class Program
+    {
+        private float[] CaliQuimica;
+        private float[] CaliFisica;
+
+        public void quimica()
+        {
+
+
+
+
+        }
+
+        public void fisica()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
