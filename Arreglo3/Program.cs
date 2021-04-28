c227303a0e9f20f46762f154b8b18abcc21ff120
Josefina Arreola <maya_aiji.11@hotmail.com>
2021-04-27 19:52:21 -0700

    Procesos completos

--- Arreglo3/Program.cs
+++ Arreglo3/Program.cs
@@ -14,7 +14,13 @@ namespace Arreglo3
         public void quimica()
         {
 
-
+            CaliQuimica = new float[4];
+            for (int i=0; i<4; i++)
+            {
+                Console.WriteLine("Ingrese la calificacion #" + (i + 1) + "de quimica:");
+                String lineaCaptura = Console.ReadLine();
+                CaliQuimica[i] = int.Parse(lineaCaptura);
+            }
 
 
         }
@@ -22,12 +28,51 @@ namespace Arreglo3
         public void fisica()
         {
 
+            CaliFisica = new float[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Ingrese la calificacion #" + (i + 1) + "de fisica:");
+                String lineaCaptura = Console.ReadLine();
+                CaliFisica[i] = int.Parse(lineaCaptura);
+            }
+        }
+
+        public void promedio()
+        {
+
+            float PromedioQ = 0;
+            float PromedioF = 0;
+            float promQ = 0;
+            float promF = 0;
+
+            for(int i=0; i<4; i++)
+            {
+                PromedioQ = PromedioQ + CaliQuimica[i];
+
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                PromedioF = PromedioF + CaliFisica[i];
+
+
+            }
+
+            promQ = PromedioQ / 5;
+            promF = PromedioF / 5;
+
+            Console.WriteLine("El primedio de las calificaciones de quimica es:" + promQ + " y el de fisica es:" + promF);
+            Console.ReadKey();
+
         }
 
         static void Main(string[] args)
         {
 
-
+            Program promedios = new Program();
+            promedios.quimica();
+            promedios.fisica();
+            promedios.promedio();
 
         }
     }
